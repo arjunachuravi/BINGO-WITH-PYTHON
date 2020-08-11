5853330052c289765469733a2d12910c288a4822
Arjun R <50737099+arjunachuravi@users.noreply.github.com>
2020-08-12 02:59:26 +0530

    Written more systamatically with Class in python

--- bingo.py
+++ bingo.py
@@ -8,16 +8,30 @@ from time import sleep
 import random
 
 class player:
-    def __init__(self,r,c,d):
-        self.r = []
-        self.c = []
-        self.d = []
+    
+    def __init__(self):
+        self.r = [] # avoid redundant row check 
+        self.c = [] # avoid redundant column check 
+        self.d = [] # avoid redundant diagonal check 
+        self.machine_player = False # player is machine or not
+        self.player_array = np.asarray(random.sample(range(1,26),25)) # bingo matrix 1-D array
+        self.player_passbook = 0 # B-I-N-G-O counter
+        self.player_name = "" # player name
+        self.player_matrix_type = "a"  # matrix fill type (a/m)
+
+    # bingo matrix 2-D array
+    def player_matrix(self):
+            return self.player_array.reshape(5,5) 
+
+    # load iter val of row to avoid redundant looping and bingo count
     def load_row(self,rx):
         self.r.append(rx)
     def load_col(self,cx):
         self.c.append(cx)
     def load_dgl(self,dx):
         self.d.append(dx)
+
+    # ckeck for iter val of row to avoid redundanct looping and bingo count
     def check_row(self,rx):
         if rx in self.r:
             return True
@@ -33,6 +47,36 @@ class player:
             return True
         else:
             return False
+    
+    # chance taker
+    def take_chance(self,NUM_PLAYERS,GAME_PLAYERS):
+        choice = 0
+        if self.machine_player == False:
+            choice = input("[INPUT] Enter the number (1-25):")
+        else:
+            choice = str(self.computerlogic())
+            print("[debug] mac-input-->",choice)
+        if choice.isnumeric() and int(choice)<26 and int(choice)>0:
+            choice = int(choice)
+            
+            if modify_grid(NUM_PLAYERS,GAME_PLAYERS,choice):
+                return
+            else:
+                if self.machine_player == False:
+                    print("[GAME] already taken")
+                self.take_chance(NUM_PLAYERS,GAME_PLAYERS)
+        else:
+            print("[ERR] invalid entry")
+            sleep(2)
+            self.take_chance(NUM_PLAYERS,GAME_PLAYERS)
+
+    # machine logic
+    def computerlogic(self):
+        choice = np.random.choice(self.player_array,1)[0] 
+        if choice == 0:
+            self.computerlogic()
+        return choice
+
 
 #function to clear screen
 def clear(): 
@@ -47,7 +91,25 @@ def exit_prgm():
     sleep(4)
     os._exit(os.X_OK)
 
-# main function 
+#display the grid 
+def grid_show(ARR):
+    print(tb(ARR,tablefmt="fancy_grid"))
+
+#if playing h-vs-h then this fn is invoked to init player objects
+def get_num_players(GAME_PLAYERS):
+    numplayer = input("[INPUT] number of players: ")
+    if numplayer.isnumeric() and int(numplayer) < 5 and int(numplayer) >= 2: # max players are set to be 5 [can be changed]
+        numplayer = int(numplayer)
+        for i in range(numplayer):
+            GAME_PLAYERS[i] = player()
+            GAME_PLAYERS[i].player_name = input("[INPUT] Enter the name of player:")
+        return numplayer
+    else:
+        print("[ERR] invalid,need to be numeric or below 5")
+        get_num_players(GAME_PLAYERS)
+    return numplayer # returns a value so that it can be used locally
+
+# Main-Menu
 def mainmenu():
     clear()
     print("thattikutt bingo 1.0 ")
@@ -56,9 +118,22 @@ def mainmenu():
     if choice.isalpha() and len(choice)==1:
         if choice in ['n','N','e','E']:
             if choice in ['n','N']:
-                new_game() 
+                print("(m)with machine \n(o)r not \n choice: ")
+                choice2 = input()
+                if choice2.isalpha() and len(choice2)==1:
+                    if choice2 in ['m','M','o','O']:
+                        if choice2 in ['m','M']:
+                            new_game(True) # play with machine
+                        else:
+                            new_game(False) # play with other humans
+                    else:
+                        print("[ERR] invalid choice")
+                        mainmenu()
+                else:
+                    print("[ERR] invalid choice")
+                    mainmenu()
             else:
-                exit_prgm()
+                exit_prgm() # exit the game
         else:
             print("[ERR] invalid choice")
             mainmenu()
@@ -66,187 +141,172 @@ def mainmenu():
         print("[ERR] invalid choice")
         mainmenu()
 
-#num players
-def get_num_players(PLAYER_NAME_DICT):
-    numplayer = input("[INPUT] number of players: ")
-    if numplayer.isnumeric() and int(numplayer) < 5 and int(numplayer) >= 2:
-        numplayer = int(numplayer)
-        for i in range(numplayer):
-            PLAYER_NAME_DICT[i] = input("[INPUT] Enter the name of player:")
-        return numplayer
-    else:
-        print("[ERR] invalid,need to be numeric or below 5")
-        get_num_players(PLAYER_NAME_DICT)
-    return numplayer
-
-#create bingo matrix manual mode
-def bingo_matrix_man(STORE_DICT,player_id):
-    temp = np.zeros(25)
+#manual entry of matrix
+def bingo_matrix_man(GAME_PLAYERS,player_id):
+    GAME_PLAYERS[player_id].player_array = np.zeros(25)
     print("[INFO] NOTE: NUMBERS WILL BE INITIALIZED BY ROWS")
     for i in range(25):
         clear()
-        grid_show(temp.reshape(5,5))
+        grid_show(GAME_PLAYERS[player_id].player_matrix())
         sleep(2)
         err = 1
         while(err):
             clear()
             num = input("[INPUT] input :")
-            if num.isnumeric() and int(num)<26 and int(num)>0 and int(num) not in temp:
+            if num.isnumeric() and int(num)<26 and int(num)>0 and int(num) not in GAME_PLAYERS[player_id].player_array:
                 num = int(num)
-                temp[i] = num
+                GAME_PLAYERS[player_id].player_array[i] = num
                 err = 0
             else:
                 print("[ERR] error @_@ type again : it may be already present ")
                 sleep(2)
                 err = 1
-    #--> store the data
-    STORE_DICT[player_id] = temp.reshape(5,5)
     print("[INFO] all done")
 
-#display the grid
-def grid_show(ARR):
-    print(tb(ARR,tablefmt="fancy_grid"))
-
-
-def new_game():
-
+# 2nd menu
+def new_game(mach_bool): # if mach_bool is True ie, play with a machine
+    GAME_PLAYERS = {} # dictionary to hold player-class objects
+    NUM_PLAYERS = 0
     clear()
+    if mach_bool == True:
+        NUM_PLAYERS = 2
+        for i in range(NUM_PLAYERS):
+            GAME_PLAYERS[i] = player()
+        GAME_PLAYERS[1].player_name = "MACHINE"
+        GAME_PLAYERS[1].machine_player = True # machine plays 2nd , human play 1st
+        GAME_PLAYERS[0].player_name = input("Enter The player's name-->")
+    else:
+        NUM_PLAYERS = get_num_players(GAME_PLAYERS)
 
-    #init
-    STORE_DICT = {}
-    PLAYER_PASSBOOK = {}
-    PLAYER_NAME_DICT = {}
-    PLAYER_DICT = {}
-    NUM_PLAYERS = get_num_players(PLAYER_NAME_DICT)
-    for i in range(NUM_PLAYERS):
-        PLAYER_PASSBOOK[i] = 0
-        PLAYER_DICT[i] = player(0,0,0)
-    VICTORY_LIST = []
-
-    #loop
-    i = 0
-    while(i >= 0 and i < NUM_PLAYERS):
-        print("options for player id:",i,"player name : ",PLAYER_NAME_DICT[i])
-        print("(a)utomatic grid generation \n(m)anual mode \n(e)xit")
-        choice = input()
-        if choice.isalpha() and len(choice)==1:
-            if choice in ['a','A','m','M','e','E']:
-                if choice in ['a','A']:
-                    ARR_MAIN = random.sample(range(1,26),25) #--> random matx
-                    ARR_MAIN = np.asarray(ARR_MAIN)
-                    STORE_DICT[i] = ARR_MAIN.reshape(5,5) 
-                elif choice in ['m','M']:
-                    bingo_matrix_man(STORE_DICT,i)
+    if mach_bool == False: # human vs human 
+        #loop
+        i = 0
+        while(i >= 0 and i < NUM_PLAYERS):
+            print("options for player id-->",i,"player name-->",GAME_PLAYERS[i].player_name)
+            print("(a)utomatic grid generation \n(m)anual mode \n(e)xit")
+            choice = input()
+            if choice.isalpha() and len(choice)==1:
+                if choice in ['a','A','m','M','e','E']:
+                    if choice in ['a','A']:
+                        GAME_PLAYERS[i].player_matrix_type = 'a'
+                    elif choice in ['m','M']:
+                        GAME_PLAYERS[i].player_matrix_type = 'm'
+                        # call the class fn to describe the matrix
+                        bingo_matrix_man(GAME_PLAYERS,i)
+                    else:
+                        exit_prgm()
+                    i = i + 1
                 else:
-                    exit_prgm()
-                i = i + 1
+                    print("[ERR] invalid choice")
             else:
                 print("[ERR] invalid choice")
-        else:
-            print("[ERR] invalid choice")
-    #--> loop ended
+        #--> loop ended
+    else:
+        #loop
+        flag = 0
+        while flag==0:
+            print("options for player id-->",i,"player name-->",GAME_PLAYERS[0].player_name)
+            print("(a)utomatic grid generation \n(m)anual mode \n(e)xit")
+            choice = input()
+            if choice.isalpha() and len(choice)==1:
+                if choice in ['a','A','m','M','e','E']:
+                    if choice in ['a','A']:
+                        GAME_PLAYERS[0].player_matrix_type = 'a'
+                    elif choice in ['m','M']:
+                        GAME_PLAYERS[0].player_matrix_type = 'm'
+                        # call the class fn to describe the matrix
+                        bingo_matrix_man(GAME_PLAYERS,0)
+                    else:
+                        exit_prgm()
+                    flag = 1
+                else:
+                    print("[ERR] invalid choice")
+            else:
+                print("[ERR] invalid choice")
+        #--> loop ended
+    play_game(mach_bool,NUM_PLAYERS,GAME_PLAYERS)
 
+#result-display function
+def display_results(VICTORY_LIST,GAME_PLAYERS):
+    header = ["Winner1","winner2","winner3","winner4","winner5"]
+    m_o_h = []
+    for i in VICTORY_LIST:
+        m_o_h.append(GAME_PLAYERS[i].player_name)
+    m_o_h = [m_o_h]
+    print(tb(m_o_h, header, tablefmt="grid"))
+
+# modify grid values
+def modify_grid(NUM_PLAYERS,GAME_PLAYERS,number):
+    flag = True
+    for iter in range(NUM_PLAYERS):
+        #find the index of number
+        result = np.where(GAME_PLAYERS[iter].player_array == number)
+        if len(result[0]) == 0:
+            flag = False
+        else:
+            pos = result[0][0]
+            GAME_PLAYERS[iter].player_array[pos] = 0
+            flag = True
+    return flag 
+
+# Logic
+def play_game(mach_bool,NUM_PLAYERS,GAME_PLAYERS):
+    VICTORY_LIST=[]
+    clear()
     print("[INFO] done setting...")
     sleep(2)
     print("[INFO] lets play")
     print("[INFO] !!! the queue will be in the order of player id !!!")
     sleep(2)
-
-    while(1):
-        flag = 1
+    while(len(VICTORY_LIST) <= NUM_PLAYERS - 1):
         i = 0
-        while(i<NUM_PLAYERS):
-            if len(VICTORY_LIST) == NUM_PLAYERS - 1:
-                display_results(VICTORY_LIST,PLAYER_NAME_DICT) #--> result display 
-                flag = 2
-                break
+        while(i < NUM_PLAYERS and len(VICTORY_LIST) <= NUM_PLAYERS - 1):
             if i not in VICTORY_LIST:
                 clear()
-                print("[INFO]","Player ",PLAYER_NAME_DICT[i]," PLAYER ID ",i," chance :")
-                take_chance(NUM_PLAYERS,STORE_DICT,i) #--> get the number , call modify_grid , then show the grid
-                check_bingo(VICTORY_LIST,PLAYER_PASSBOOK,STORE_DICT,NUM_PLAYERS,PLAYER_DICT) #--> checks if player won at the instance returns t/f
-                grid_show(STORE_DICT[i])
-                sleep(2)
-                i = i + 1
-        if flag == 2:
-            break
+                print("[INFO]","Player-->",GAME_PLAYERS[i].player_name," PLAYER ID-->",i," chance-->")
+                GAME_PLAYERS[i].take_chance(NUM_PLAYERS,GAME_PLAYERS)
+                grid_show(GAME_PLAYERS[i].player_matrix())
+                check_bingo(VICTORY_LIST,NUM_PLAYERS,GAME_PLAYERS) #--> checks if player won
+                sleep(5)
+            i = i + 1
     sleep(5)
 
-    mainmenu()
+    mainmenu() # jumps to main-menu fun [recursive]
 
-#modify the grid --> when player calls a number it is turned to zero
-def modify_grid(NUM_PLAYERS,STORE_DICT,number):
-    flag = True
-    for iter in range(NUM_PLAYERS):
-        #find the index of number
-        result = np.where(STORE_DICT[iter] == number)
-        if len(result[0]) == 0:
-            flag = False
-        else:
-            i,j = result[0][0],result[1][0]
-            #traditional init
-            temp = STORE_DICT[iter]
-            temp[i][j] = 0
-            STORE_DICT[iter] = temp
-            flag = True
-    return flag 
-
-#--> player chance alternator
-def take_chance(NUM_PLAYERS,STORE_DICT,player_id):
-    choice = input("[INPUT] Enter the number (1-25):")
-    if choice.isnumeric() and int(choice)<26 and int(choice)>0:
-        choice = int(choice)
-        if modify_grid(NUM_PLAYERS,STORE_DICT,choice):
-            return
-        else:
-            print("[GAME] already taken")
-            take_chance(NUM_PLAYERS,STORE_DICT,player_id)
-    else:
-        print("[ERR] invalid entry")
-        sleep(2)
-        take_chance(NUM_PLAYERS,STORE_DICT,player_id)
-
-def display_results(VICTORY_LIST,PLAYER_NAME_DICT):
-    header = ["Winner1","winner2","winner3","winner4","winner5"]
-    m_o_h = []
-    for i in VICTORY_LIST:
-        m_o_h.append(PLAYER_NAME_DICT[i])
-    m_o_h = [m_o_h]
-    print(tb(m_o_h, header, tablefmt="grid"))
-
-def check_bingo(VICTORY_LIST,PLAYER_PASSBOOK,STORE_DICT,NUM_PLAYERS,PLAYER_DICT):
+def check_bingo(VICTORY_LIST,NUM_PLAYERS,GAME_PLAYERS):
     for player_id in range(NUM_PLAYERS):
-        temp = STORE_DICT[player_id].flatten()
-
         for i in range(5):
-            if temp[i+0] == temp[i+5] == temp[i+10] == temp[i+15] == temp[i+20]:
-                if PLAYER_DICT[player_id].check_col(i) == False:
-                    PLAYER_DICT[player_id].load_col(i)
-                    PLAYER_PASSBOOK[player_id] = PLAYER_PASSBOOK[player_id] + 1
-                    if PLAYER_PASSBOOK[player_id] == 5:
+            if GAME_PLAYERS[player_id].player_array[i+0] == GAME_PLAYERS[player_id].player_array[i+5] == GAME_PLAYERS[player_id].player_array[i+10] == GAME_PLAYERS[player_id].player_array[i+15] == GAME_PLAYERS[player_id].player_array[i+20]:
+                if GAME_PLAYERS[player_id].check_col(i) == False:
+                    GAME_PLAYERS[player_id].load_col(i)
+                    GAME_PLAYERS[player_id].player_passbook = GAME_PLAYERS[player_id].player_passbook + 1
+                    if GAME_PLAYERS[player_id].player_passbook == 5:
                         VICTORY_LIST.append(player_id)
 
         for i in range(0,21,5):
-            if temp[i+0] == temp[i+1] == temp[i+2] == temp[i+3] == temp[i+4]:
-                if PLAYER_DICT[player_id].check_row(i) == False:
-                    PLAYER_DICT[player_id].load_row(i)
-                    PLAYER_PASSBOOK[player_id] = PLAYER_PASSBOOK[player_id] + 1
-                    if PLAYER_PASSBOOK[player_id] == 5:
+            if GAME_PLAYERS[player_id].player_array[i+0] == GAME_PLAYERS[player_id].player_array[i+1] == GAME_PLAYERS[player_id].player_array[i+2] == GAME_PLAYERS[player_id].player_array[i+3] == GAME_PLAYERS[player_id].player_array[i+4]:
+                if GAME_PLAYERS[player_id].check_row(i) == False:
+                    GAME_PLAYERS[player_id].load_row(i)
+                    GAME_PLAYERS[player_id].player_passbook = GAME_PLAYERS[player_id].player_passbook + 1
+                    if GAME_PLAYERS[player_id].player_passbook == 5:
                         VICTORY_LIST.append(player_id)
 
-        if PLAYER_DICT[player_id].check_dgl(0) == False:
-            if temp[0] == temp[6] == temp[12] == temp[18] == temp[24]:
-                    PLAYER_DICT[player_id].load_dgl(0)
-                    PLAYER_PASSBOOK[player_id] = PLAYER_PASSBOOK[player_id] + 1
-                    if PLAYER_PASSBOOK[player_id] == 5:
+        if GAME_PLAYERS[player_id].check_dgl(0) == False:
+            if GAME_PLAYERS[player_id].player_array[0] == GAME_PLAYERS[player_id].player_array[6] == GAME_PLAYERS[player_id].player_array[12] == GAME_PLAYERS[player_id].player_array[18] == GAME_PLAYERS[player_id].player_array[24]:
+                    GAME_PLAYERS[player_id].load_dgl(0)
+                    GAME_PLAYERS[player_id].player_passbook = GAME_PLAYERS[player_id].player_passbook + 1
+                    if GAME_PLAYERS[player_id].player_passbook == 5:
                         VICTORY_LIST.append(player_id)
 
-        if PLAYER_DICT[player_id].check_dgl(4) == False:
-            if temp[4] == temp[8] == temp[12] == temp[16] == temp[20]:
-                    PLAYER_DICT[player_id].load_dgl(4)
-                    PLAYER_PASSBOOK[player_id] = PLAYER_PASSBOOK[player_id] + 1
-                    if PLAYER_PASSBOOK[player_id] == 5:
+        if GAME_PLAYERS[player_id].check_dgl(4) == False:
+            if GAME_PLAYERS[player_id].player_array[4] == GAME_PLAYERS[player_id].player_array[8] == GAME_PLAYERS[player_id].player_array[12] == GAME_PLAYERS[player_id].player_array[16] == GAME_PLAYERS[player_id].player_array[20]:
+                    GAME_PLAYERS[player_id].load_dgl(4)
+                    GAME_PLAYERS[player_id].player_passbook = GAME_PLAYERS[player_id].player_passbook + 1
+                    if GAME_PLAYERS[player_id].player_passbook == 5:
                         VICTORY_LIST.append(player_id)
+        
+        if len(VICTORY_LIST) == NUM_PLAYERS - 1:
+                display_results(VICTORY_LIST,GAME_PLAYERS) #--> result display 
 
 if __name__ == "__main__":
     mainmenu()
